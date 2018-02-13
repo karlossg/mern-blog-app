5349f2d9393164bbcc26224eacc66da45543ac75
karlossg <karol.gratus@gmail.com>
2018-02-13 19:23:13 +0100

    thumb up and down actions

--- client/modules/Post/PostActions.js
+++ client/modules/Post/PostActions.js
@@ -5,6 +5,8 @@ export const ADD_POST = 'ADD_POST';
 export const ADD_POSTS = 'ADD_POSTS';
 export const DELETE_POST = 'DELETE_POST';
 export const EDIT_POST = 'EDIT_POST';
+export const THUMB_UP_POST = 'THUMB_UP_POST';
+export const THUMB_DOWN_POST = 'THUMB_DOWN_POST';
 
 // Export Actions
 export function addPost(post) {
@@ -79,3 +81,17 @@ export function editPostRequest(cuid, post) {
     }).then(() => dispatch(editPost(cuid, post)));
   };
 }
+
+export const thumbUpPost = (cuid) => {
+  return {
+    type: THUMB_UP_POST,
+    cuid,
+  };
+};
+
+export const thumbDownPost = (cuid) => {
+  return {
+    type: THUMB_DOWN_POST,
+    cuid,
+  };
+};
